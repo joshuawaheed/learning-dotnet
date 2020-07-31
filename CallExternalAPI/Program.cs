af053c69b202da021ce56f7543b9f0bbea35d6c6
Joshua Waheed <waheedj@parliament.uk>
2020-07-31 10:53:23 +0100

    Do a GET request

--- CallExternalAPI/Program.cs
+++ CallExternalAPI/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 
 namespace CallExternalAPI
 {
@@ -6,7 +8,32 @@ namespace CallExternalAPI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Get();
+        }
+
+        private static void Get()
+        {
+            string stringUrlTest = string.Format("http://jsonplaceholder.typicode.com/posts/1/comments");
+
+            WebRequest requestObject = WebRequest.Create(stringUrlTest);
+
+            requestObject.Method = "GET";
+
+            HttpWebResponse responseObject = null;
+
+            responseObject = (HttpWebResponse)requestObject.GetResponse();
+
+            string stringResultTest = null;
+
+            using (Stream stream = responseObject.GetResponseStream())
+            {
+                StreamReader streamReader = new StreamReader(stream);
+                stringResultTest = streamReader.ReadToEnd();
+                streamReader.Close();
+            }
+
+            Console.WriteLine(stringResultTest);
+            Console.Read();
         }
     }
 }
