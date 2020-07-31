0d05024177ff581a805f87bf945ebf3dcfd9ea5e
Joshua Waheed <waheedj@parliament.uk>
2020-07-31 11:18:51 +0100

    Do a POST request

--- CallExternalAPI/Program.cs
+++ CallExternalAPI/Program.cs
@@ -8,12 +8,12 @@ namespace CallExternalAPI
     {
         static void Main(string[] args)
         {
-            Get();
+            Post();
         }
 
         private static void Get()
         {
-            string stringUrlTest = string.Format("http://jsonplaceholder.typicode.com/posts/1/comments");
+            string stringUrlTest = string.Format("https://jsonplaceholder.typicode.com/posts/1/comments");
 
             WebRequest requestObject = WebRequest.Create(stringUrlTest);
 
@@ -34,6 +34,46 @@ namespace CallExternalAPI
 
             Console.WriteLine(stringResultTest);
             Console.Read();
+        }
+
+        private static void Post()
+        {
+            string url = "https://jsonplaceholder.typicode.com/posts";
+            
+            string stringUrlTest = string.Format(url);
+
+            WebRequest requestObject = WebRequest.Create(stringUrlTest);
+
+            requestObject.Method = "POST";
+
+            requestObject.ContentType = "application/json";
+
+            string postData = "{" +
+                "\"title\": \"joshuaWaheedTestTitle\"," +
+                "\"body\": \"joshuaWaheedTestBody\"," +
+                "\"userId\": \"50\"" +
+            "}";
+
+            using (var streamWriter = new StreamWriter(requestObject.GetRequestStream()))
+            {
+                streamWriter.Write(postData);
+                streamWriter.Flush();
+                streamWriter.Close();
+
+                var responseObject = requestObject.GetResponse();
+
+                string stringResultTest = null;
+
+                using (Stream stream = responseObject.GetResponseStream())
+                {
+                    StreamReader streamReader = new StreamReader(stream);
+                    stringResultTest = streamReader.ReadToEnd();
+                    streamReader.Close();
+                }
+
+                Console.WriteLine(stringResultTest);
+                Console.Read();
+            }
         }
     }
 }
