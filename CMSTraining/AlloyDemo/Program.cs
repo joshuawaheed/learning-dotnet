de8a3e93d5cd47e1cf9a7914f1bc3a6f588b1c83
joshuawaheed <joshua.waheed@gmail.com>
2024-07-01 12:48:47 +0100

    Exercise G3 - Configuring multi-site

--- CMSTraining/AlloyDemo/Program.cs
+++ CMSTraining/AlloyDemo/Program.cs
@@ -7,5 +7,14 @@ public class Program
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
             .ConfigureCmsDefaults()
-            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+            // tell Kestrel to listen on 127.0.0.1 for requests to
+            // port 801 e.g. http://www.alloy.com:801/
+            .ConfigureWebHost(options =>
+            {
+                options.ConfigureKestrel(options =>
+                {
+                    options.Listen(System.Net.IPAddress.Loopback, 801);
+                });
+            });
 }
